b9f47d689d041c641ab472879063310aca1dbca5
SMB開発部 <sorimachi-aws@hotmail.com>
2014-11-27 14:15:05 +0900

    空白

--- chapter8-2/MasterViewController.swift
+++ chapter8-2/MasterViewController.swift
@@ -48,6 +48,7 @@ class MasterViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
     // MARK: - Segues
